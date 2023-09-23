e90ea19b061cd8b633c81537d9c316bd69eba83a
Miguel Condori <miguel.angel.condori@gmail.com>
2023-09-23 14:07:33 -0300

    lesson2

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-const awesomeFunction = (req, res, next) => {
-    res.json("Claudia Dominguez");
-};
-
-const returnAnotherPerson = (req, res, next) => {
-    res.json("Super awesome person");
-};
-
-module.exports = {awesomeFunction, returnAnotherPerson};
