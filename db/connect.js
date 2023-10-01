04cd4403d2777c0223b36aeb2f9e35f1cd9b1608
Miguel Condori <miguel.angel.condori@gmail.com>
2023-10-01 00:35:59 -0300

    change in .env

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,6 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const MongoClient = require('mongodb').MongoClient;
-const MONGODB_URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.hmvk6uf.mongodb.net/?authSource=admin`;
 
 let _db;
 
